5a80d48a2a37414e3c317a07af11b9ef2f79c427
Oleksii Hostryk <125027886+Alex1Go@users.noreply.github.com>
2025-10-05 21:23:04 +0300

    test

--- js/module.js
+++ js/module.js
@@ -1,3 +1,65 @@
+// export function initModuleToggle() {
+//   const infos = document.querySelectorAll('.info');
+
+//   infos.forEach(info => {
+//     const box = info.querySelector('.info__box');
+//     const btnItem = info.querySelector('.info__btn-item');
+
+//     box.addEventListener('click', () => {
+//       infos.forEach(el => {
+//         if (el !== info) {
+//           el.classList.remove('open');
+//           const elBtn = el.querySelector('.info__btn-item');
+//           if (elBtn) elBtn.textContent = 'Більше';
+//         }
+//       });
+
+//       info.classList.toggle('open');
+
+//       if (info.classList.contains('open')) {
+//         btnItem.textContent = 'Менше';
+
+//         const firstLi = info.querySelector('.info__content-list li');
+//         if (firstLi) {
+//           setActiveLesson(firstLi, info);
+//         }
+//       } else {
+//         btnItem.textContent = 'Більше';
+//       }
+//     });
+//   });
+// }
+
+// export function initLessonHover() {
+//   document.querySelectorAll('.info__content-list li').forEach(item => {
+//     item.addEventListener('mouseenter', () => {
+//       const parent = item.closest('.info');
+//       setActiveLesson(item, parent);
+//     });
+//   });
+// }
+
+// function setActiveLesson(item, parent) {
+//   const id = item.getAttribute('data-id');
+
+//   parent.querySelectorAll('.info__content-list li').forEach(li => li.classList.remove('active'));
+
+//   item.classList.add('active');
+
+//   parent
+//     .querySelectorAll('.info__detail-list')
+//     .forEach(detail => detail.classList.remove('active'));
+
+//   const activeDetail = parent.querySelector(`.info__detail-list[data-id="${id}"]`);
+//   if (activeDetail) {
+//     activeDetail.classList.add('active');
+//   }
+// }
+
+// export function initModule() {
+//   initModuleToggle();
+//   initLessonHover();
+// }
 export function initModuleToggle() {
   const infos = document.querySelectorAll('.info');
 
@@ -30,9 +92,9 @@ export function initModuleToggle() {
   });
 }
 
-export function initLessonHover() {
+export function initLessonClick() {
   document.querySelectorAll('.info__content-list li').forEach(item => {
-    item.addEventListener('mouseenter', () => {
+    item.addEventListener('click', () => {
       const parent = item.closest('.info');
       setActiveLesson(item, parent);
     });
@@ -44,6 +106,7 @@ function setActiveLesson(item, parent) {
 
   parent.querySelectorAll('.info__content-list li').forEach(li => li.classList.remove('active'));
 
+  // Активируем выбранную
   item.classList.add('active');
 
   parent
@@ -58,5 +121,5 @@ function setActiveLesson(item, parent) {
 
 export function initModule() {
   initModuleToggle();
-  initLessonHover();
+  initLessonClick();
 }
